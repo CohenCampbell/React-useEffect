e47bfc3f2a9fc10be3d976c70ab6342b2a22c0e1
someguy <someguy@TheSuperUltraOmegaPhoneWavePremium>
2024-02-08 23:10:11 -0500

    Committing finished project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Deck from "./Deck";
+import Card from "./Card"
+
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const cardHtml = cards.map(card => <Card card={card}/>)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Deck setCards={setCards}/>
+      {cardHtml}
     </div>
   );
 }
